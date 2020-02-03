7414b8ba405ffc20b1e1dc2952cbdcc05ee5978d
Guillaume Quint <g.quint@studenti.unipi.it>
2020-02-03 18:47:15 +0100

    Aggiunta seconda operazione

--- implementazione_mysql/Operazioni.sql
+++ implementazione_mysql/Operazioni.sql
@@ -65,7 +65,8 @@ Descrizione:Viene controllato che il pasto che viene somministrato agli animali
     nei quali è stato somministrato il pasto in esame
 Input:composizione del pasto
 Output:media degli indici di salute
-Frequenza giornaliera:33*/
+Frequenza giornaliera:33
+*/
 
 /* Magari può essere utile un trigger da attivare ogni volta che viene cambiato pasto in un locale...
 DROP TRIGGER IF EXISTS cambio_pasto;
@@ -156,4 +157,132 @@ GROUP BY L.codiceLocale;
 END $$
 DELIMITER ;
 
+/*-------------------------------------------------------------
 
+/*
+Operazione 2: Registrazione di una ripoduzione con successo
+Descrizione:Al completamento di una riproduzione terminata con succeso, viene
+	compilata l’anagrafica del neonato, viene aggiornato lo stato della riproduzione e
+    viene programmata una visita per il nuovo nato
+Input:codice della riproduzione avvenuta con successo e dati appartenenti al
+	neonato (sesso, specie, famiglia, razza, peso, altezza)
+Output:registrazione del neonato con programmazione di una nuova visita
+Frequenza giornaliera:32
+*/
+
+DELIMITER $$
+CREATE FUNCTION first_available_code() 
+RETURNS SMALLINT UNSIGNED
+DETERMINISTIC
+BEGIN
+    DECLARE return_value SMALLINT UNSIGNED;
+	SET return_value = 0;
+	WHILE (SELECT A.codice
+			FROM Animale A
+            WHERE codice = return_value) IS NOT NULL DO
+		SET return_value = return_value + 1;
+	END WHILE;
+    RETURN (return_value);
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE FUNCTION first_available_GPS() 
+RETURNS SMALLINT UNSIGNED
+DETERMINISTIC
+BEGIN
+    DECLARE return_value SMALLINT UNSIGNED;
+	SET return_value = 0;
+	WHILE (SELECT A.codiceGPS
+			FROM Animale A
+            WHERE codiceGPS = return_value) IS NOT NULL DO
+		SET return_value = return_value + 1;
+	END WHILE;
+    RETURN (return_value);
+END$$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS check_stato_riproduzione;
+DELIMITER $$
+CREATE TRIGGER check_stato_riproduzione
+AFTER UPDATE ON Riproduzione
+FOR EACH ROW 
+BEGIN 
+IF NEW.stato = 'insuccesso' THEN
+INSERT INTO
+	Visita(descrizione,
+			dataProgrammata,
+			codAnimale,
+			codVeterinario)
+	VALUES('Visita di controllo automaticamente richiesta in seguito alla riproduzione fallita da parte dell\'animale',
+			CURRENT_DATE + INTERVAL 3 DAY,
+            NEW.codiceMadre,
+            (SELECT R.codVeterinario
+				FROM Riproduzione R
+				WHERE R.codiceRiproduzione = NEW.codRiproduzione));
+ELSE IF NEW.stato = 'successo' THEN
+	signal sqlstate '70006' SET MESSAGE_TEXT = 'Chiamare la procedure OP2_registrazione_neonato per registrare il neonato e prenotargli una visita';
+END IF;
+SET NEW.attributo = ();
+END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE OP2_registrazione_neonato
+	(IN _codRiproduzione SMALLINT UNSIGNED,
+    IN _sesso	ENUM('M', 'F'),
+	IN _specie	VARCHAR(30),
+	IN _famiglia	VARCHAR(30),
+	IN _dataDiNascita	DATE,
+	IN _altezza	TINYINT UNSIGNED, /*Si suppone che nessun animale superi i 255 cm in altezza*/
+	IN _peso	SMALLINT UNSIGNED, /*Si suppone che nessun animale superi le 65 tonnelate*/
+	IN _razza	VARCHAR(30))
+BEGIN
+  DECLARE codice_ SMALLINT UNSIGNED;
+  DECLARE GPS_ SMALLINT UNSIGNED;
+  SET codice_ = first_available_code();
+  SET GPS_ = first_available_GPS();
+  INSERT INTO 
+  Animale(codice,
+			sesso,
+			specie,
+			famiglia,
+			idMadre,
+			idPadre,
+			dataDiNascita,
+			altezza,
+			peso,
+			razza,
+			codiceGPS,
+			codLocale)
+	VALUES(codice_,
+			_sesso,
+			_specie,
+			_famiglia,
+			(SELECT R.codiceMadre
+				FROM Riproduzione R
+				WHERE R.codiceRiproduzione = _codRiproduzione),
+			(SELECT R.codicePadre
+				FROM Riproduzione R
+				WHERE R.codiceRiproduzione = _codRiproduzione),
+			_dataDiNascita,
+			_altezza,
+			_peso,
+			_razza,
+			GPS_,
+			(SELECT A.codLocale /*il neonato abita il locale della madre*/
+				FROM Animale A INNER JOIN Riproduzione R ON A.codice = R.codiceMadre
+				WHERE R.codiceRiproduzione = _codRiproduzione)); 
+  INSERT INTO 
+	Visita(descrizione,
+			dataProgrammata,
+			codAnimale,
+			codVeterinario)
+	VALUES('Visita di controllo automaticamente richiesta in seguito alla nascita dell\' animale',
+			CURRENT_DATE + INTERVAL 3 DAY,
+            codice_,
+            (SELECT R.codVeterinario
+				FROM Riproduzione R
+				WHERE R.codiceRiproduzione = _codRiproduzione));
+END $$
+DELIMITER ;
