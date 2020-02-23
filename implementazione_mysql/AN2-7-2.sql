ab0a5769f795f3f8da769ad26aef0f08fec55ca9
Guillaume Quint <g.quint@studenti.unipi.it>
2020-02-23 16:06:00 +0100

    avanzamento sull'analytic e il relativo test_script

--- implementazione_mysql/AN2-7-2.sql
+++ implementazione_mysql/AN2-7-2.sql
@@ -1,5 +1,27 @@
 WITH DevianzaFase AS (
-SELECT F.codiceFase, 
-FROM ControlloParametriFase CPF INNER JOIN Fasi F ON CPF.codFase = F.codiceFase)
-SELECT *
-FROM DevianzaFase;
+SELECT CPF.codLotto, F.codiceFase, 2*ABS(CPF.parametriEffettivi - F.parametriProcesso)/(CPF.parametriEffettivi + F.parametriProcesso) AS DifferenzaRelativa -- TODO:da ripetere per ogni parametro da controllare!!
+FROM ControlloParametriFase CPF INNER JOIN Fasi F ON CPF.codFase = F.codiceFase),
+DevianzaParametriCantine AS (
+SELECT SC.codLotto, P.codCantina, STD(P.temperatura) AS temperatura, STD(P.umidità) AS umidità
+FROM stoccaggiocantine SC INNER JOIN Parametri P ON SC.codCantina = P.codCantina
+GROUP BY P.codCantina),
+SaluteMediaLocale AS (
+	SELECT SM.*, AnimaliProduttori.codLotto 
+	FROM (SELECT Ins.codAnimale, 
+			AVG(Ins.tipologiaRespirazione) AS tipologiaRespirazione, 
+            AVG(Ins.lucentezzaPelo) AS lucentezzaPelo, 
+            AVG(Ins.vigilanza) AS vigilanza, 
+            AVG(Ins.idratazione) AS idratazione, 
+            AVG(Ins.deambulazione) AS deambulazione
+		FROM indicisalute InS
+		GROUP BY InS.codAnimale) SM INNER JOIN (SELECT Distinct(A.codice) AS codAnimale, PC.codLotto
+									FROM prodottocon PC INNER JOIN Latte La ON PC.codLatte = La.codiceLatte
+										INNER JOIN Animale A ON La.codAnimale = A.codice) AnimaliProduttori
+									ON SM.codAnimale = AnimaliProduttori.codAnimale
+)
+SELECT Lo.codiceLotto, DF.codiceFase, DF.differenzaRelativa, STD(La.quantitàSostanzeDisciolte), AVG(DPC.temperatura), AVG(DPC.umidità), AVG(SML.tipologiaRespirazione), AVG(SML.lucentezzaPelo), AVG(SML.vigilanza), AVG(SML.idratazione), AVG(SML.deambulazione)
+FROM Lotto Lo INNER JOIN DevianzaFase DF ON Lo.codiceLotto = DF.codLotto
+	INNER JOIN Latte La ON La.codiceLatte IN (SELECT PC.codLatte FROM prodottocon PC WHERE PC.codLotto = Lo.codiceLotto)
+    INNER JOIN DevianzaParametriCantine DPC ON Lo.codiceLotto = DPC.codLotto
+    INNER JOIN SaluteMediaLocale SML ON Lo.codiceLotto = SML.codLotto
+GROUP BY Lo.codiceLotto
--- implementazione_mysql/test-sets/test_AN2-7-2/test_script.sql
+++ implementazione_mysql/test-sets/test_AN2-7-2/test_script.sql
@@ -2,19 +2,30 @@ INSERT INTO Animale (codice, sesso, specie, famiglia, idMadre, idPadre, dataDiNa
 	VALUES (20, 'F', 'bos lobe', 'tetracerus', 2, 1, '1977-05-19', 92, 191, 'tedesca', 117.249, -178.780, '2019-03-20 18:13:15', 3, 1);
 INSERT INTO Animale (codice, sesso, specie, famiglia, idMadre, idPadre, dataDiNascita, altezza, peso, razza, latitudine, longitudine, orario, codiceGps, codLocale)
 	VALUES (19, 'F', 'bos lobe', 'tetracerus', 2, 1, '1977-05-19', 92, 191, 'tedesca', 117.249, -178.780, '2019-03-20 18:13:15', 3, 1);
+INSERT INTO Animale (codice, sesso, specie, famiglia, idMadre, idPadre, dataDiNascita, altezza, peso, razza, latitudine, longitudine, orario, codiceGps, codLocale)
+	VALUES (18, 'F', 'bos lobe', 'tetracerus', 2, 1, '1977-05-19', 92, 191, 'tedesca', 117.249, -178.780, '2019-03-20 18:13:15', 3, 1);
 
+/*animale piuttosto sano*/
 INSERT INTO Indicisalute (codAnimale, dataRilevazione, tipologiaRespirazione, lucentezzaPelo, vigilanza, idratazione, deambulazione, fibre, proteine, glucidi, concentrazioneSali, concentrazioneVitamine)
 	VALUES (20, '2019-02-07', 92, 94, 95, 98, 92, 103, 967, 878, 10, 5);
 INSERT INTO Indicisalute (codAnimale, dataRilevazione, tipologiaRespirazione, lucentezzaPelo, vigilanza, idratazione, deambulazione, fibre, proteine, glucidi, concentrazioneSali, concentrazioneVitamine)
 	VALUES (20, '2019-02-06', 93, 98, 95, 95, 91, 103, 967, 878, 10, 5);
 INSERT INTO Indicisalute (codAnimale, dataRilevazione, tipologiaRespirazione, lucentezzaPelo, vigilanza, idratazione, deambulazione, fibre, proteine, glucidi, concentrazioneSali, concentrazioneVitamine)
 	VALUES (20, '2019-02-05', 92, 95, 95, 98, 92, 103, 967, 878, 10, 5);
+/*animale frequentemente malato*/
 INSERT INTO Indicisalute (codAnimale, dataRilevazione, tipologiaRespirazione, lucentezzaPelo, vigilanza, idratazione, deambulazione, fibre, proteine, glucidi, concentrazioneSali, concentrazioneVitamine)
 	VALUES (19, '2019-02-04', 12, 34, 95, 47, 82, 103, 967, 878, 10, 5);
 INSERT INTO Indicisalute (codAnimale, dataRilevazione, tipologiaRespirazione, lucentezzaPelo, vigilanza, idratazione, deambulazione, fibre, proteine, glucidi, concentrazioneSali, concentrazioneVitamine)
 	VALUES (19, '2019-02-03', 12, 38, 55, 48, 82, 103, 967, 878, 10, 5);
 INSERT INTO Indicisalute (codAnimale, dataRilevazione, tipologiaRespirazione, lucentezzaPelo, vigilanza, idratazione, deambulazione, fibre, proteine, glucidi, concentrazioneSali, concentrazioneVitamine)
 	VALUES (19, '2019-02-02', 12, 34, 95, 48, 62, 103, 967, 878, 10, 5);
+/*animale che partecipa alla produzione di un altro lotto*/
+INSERT INTO Indicisalute (codAnimale, dataRilevazione, tipologiaRespirazione, lucentezzaPelo, vigilanza, idratazione, deambulazione, fibre, proteine, glucidi, concentrazioneSali, concentrazioneVitamine)
+	VALUES (18, '2019-02-04', 99, 99, 99, 99, 99, 103, 967, 878, 10, 5);
+INSERT INTO Indicisalute (codAnimale, dataRilevazione, tipologiaRespirazione, lucentezzaPelo, vigilanza, idratazione, deambulazione, fibre, proteine, glucidi, concentrazioneSali, concentrazioneVitamine)
+	VALUES (18, '2019-02-03', 98, 98, 98, 98, 98, 103, 967, 878, 10, 5);
+INSERT INTO Indicisalute (codAnimale, dataRilevazione, tipologiaRespirazione, lucentezzaPelo, vigilanza, idratazione, deambulazione, fibre, proteine, glucidi, concentrazioneSali, concentrazioneVitamine)
+	VALUES (18, '2019-02-02', 97, 97, 97, 97, 97, 103, 967, 878, 10, 5);
 
 INSERT INTO Latte (codiceLatte, quantitàSostanzeDisciolte, codAnimale, codSilos)
 	VALUES (5, 62, 20, 0);
@@ -26,6 +37,18 @@ INSERT INTO Latte (codiceLatte, quantitàSostanzeDisciolte, codAnimale, codSilos
 	VALUES (8, 22, 19, 0);
 INSERT INTO Latte (codiceLatte, quantitàSostanzeDisciolte, codAnimale, codSilos)
 	VALUES (9, 32, 20, 0);
+INSERT INTO Latte (codiceLatte, quantitàSostanzeDisciolte, codAnimale, codSilos)
+	VALUES (10, 99, 18, 0);
+INSERT INTO Latte (codiceLatte, quantitàSostanzeDisciolte, codAnimale, codSilos) -- non usato per alcuna produzione
+	VALUES (11, 99, 18, 0);
+
+
+INSERT INTO Lotto (codiceLotto, codDipendenti, dataProd, codLab, durata)
+	VALUES (10, 'Scodice dei dipendenti del primo lotto', '2019-04-07', 10, '10:57:01');
+INSERT INTO Lotto (codiceLotto, codDipendenti, dataProd, codLab, durata)
+	VALUES (11, 'codice dei dipendenti del secondo lotto', '2019-04-08', 11, '10:57:01');
+INSERT INTO Lotto (codiceLotto, codDipendenti, dataProd, codLab, durata)
+	VALUES (12, 'codice dei dipendenti del terzo lotto', '2019-04-06', 12, '10:57:01');
 
 INSERT INTO prodottocon (codLatte, codLotto)
 	VALUES (5, 10);
@@ -37,8 +60,18 @@ INSERT INTO prodottocon (codLatte, codLotto)
 	VALUES (8, 11);
 INSERT INTO prodottocon (codLatte, codLotto)
 	VALUES (9, 11);
-
-INSERT INTO Lotto (codiceLotto, codDipendenti, dataProd, codLab, durata)
-	VALUES (10, 'Scodice dei dipendenti del primo lotto', '2019-04-07', 10, '10:57:01');
-INSERT INTO Lotto (codiceLotto, codDipendenti, dataProd, codLab, durata)
-	VALUES (11, 'codice dei dipendenti del secondo lotto', '2019-04-08', 11, '10:57:01');
+INSERT INTO prodottocon (codLatte, codLotto)
+	VALUES (10, 12);
+/*
+INSERT INTO Fasi (codiceFase, nome, parametriProcesso, durata)
+	VALUES (50, 'Inizio', 57, '23:22:48');
+INSERT INTO ControlloParametriFase (parametriEffettivi, codLotto, codFase)
+	VALUES (59, 1, 1);
+INSERT INTO Ricetta (codiceRicetta, collocazioneGeografica, ricetta, durataStagionatura)
+	VALUES (30, 'Veneto', 'ricetta prima', '20:33:55');
+INSERT INTO divisain (codRicetta, codFase)
+	VALUES (1, 1);
+INSERT INTO Formaggio (nome, nomeAgriturismo, deperibilita, codRicetta, codLatte)
+	VALUES ('Parmigiano', 'Settesoldi', 30, 30);
+INSERT INTO composizioneformaggio ('Parmigiano', 'Settesoldi', 
+*/
